a025ec310e6442d9f2a8433b19b5555bad556937
Daffie95 <patrik.svahnstrom@gmail.com>
2021-04-05 15:21:57 +0000

    configured customer adding items to shopping bag

--- bag/views.py
+++ bag/views.py
@@ -10,7 +10,8 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the product to the shopping bag """
+    """ Add a quantity of the product to the shopping bag
+        using HTTP session """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
