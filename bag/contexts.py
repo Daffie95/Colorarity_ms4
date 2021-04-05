9b1207e9447eba25d2a3e26f1da2485fbd62f7a2
Daffie95 <patrik.svahnstrom@gmail.com>
2021-04-05 15:32:00 +0000

    configured shopping bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,10 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+""" function to define and show
+    shopping bag items thats been added """
 
 
 def bag_contents(request):
@@ -7,6 +12,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, queantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += queantity * product.price
+        product_count += queantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_SHIPPING_THRESHOLD:
         shipping = total * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
