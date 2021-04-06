af06b806240d753b8d61c21587f592fedbce8ff5
Daffie95 <patrik.svahnstrom@gmail.com>
2021-04-06 07:36:13 +0000

    configured views for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -4,3 +4,16 @@ from django.contrib import messages
 from .forms import OrderForm
 
 # Create your views here.
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your shopping bag is empty")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
