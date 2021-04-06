954aa0360076b9632922e848c69e5239f49666b0
Daffie95 <patrik.svahnstrom@gmail.com>
2021-04-06 08:43:00 +0000

    configuring stripe

--- checkout/static/checkout/js/sttripe_elements.js
+++ checkout/static/checkout/js/sttripe_elements.js
@@ -8,8 +8,8 @@
 
 
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret_key = $('#id_client_secret_key').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key)
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
     base: {
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IdA8fJBuBWM0lqnHjgDevg6Qaw2TrILkgfPQm6q081v2zHuM0RnoXpRclO5E4As33NiYIHomLY1IfQNLwgdfCb900Ygjmagbp',
+        'client_secret_key': 'sk_test_51IdA8fJBuBWM0lqnZW1F4YegLU76ClYxVthDaIwVJVn3QwrsDULgO4tKzSZu2isGn2AFV0ZE1zt0Nze0qHcupnMz00wmnp8AA1'
+
     }
 
     return render(request, template, context)
